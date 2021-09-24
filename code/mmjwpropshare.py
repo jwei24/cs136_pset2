338a962aa6a8cfcd039767c80af11c6899207555
mosesmayer <mosesmayer@gmail.com>
2021-09-23 21:44:18 -0400

    propshare_attempt1

--- code/mmjwpropshare.py
+++ code/mmjwpropshare.py
@@ -13,6 +13,8 @@ from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
+from math import floor
+
 class MMJWPropshare(Peer):
     def post_init(self):
         print(("post_init(): %s here!" % self.id))
@@ -53,6 +55,7 @@ class MMJWPropshare(Peer):
         peers.sort(key=lambda p: p.id)
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+        piece_counts = {}
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
@@ -60,13 +63,31 @@ class MMJWPropshare(Peer):
             # More symmetry breaking -- ask for random pieces.
             # This would be the place to try fancier piece-requesting strategies
             # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
+            for piece_id in list(isect):
+                if not piece_id in piece_counts:
+                    piece_counts[piece_id] = 0
+                piece_counts[piece_id] += 1
+        rarest_first = list(piece_counts.items())[:]
+        random.shuffle(rarest_first)
+        rarest_first = sorted(rarest_first, key = lambda x: x[1])
+        # logging.debug("Piece set for {} = {}".format(self.id, self.pieces))
+        for peer in peers:
+            cur_len = 0
+            for piece_id, _counts in rarest_first:
+                if cur_len >= self.max_requests:
+                    break
+                av_set = set(peer.available_pieces)
+                isect = av_set.intersection(np_set)
+                isect_list = list(isect)
+                if not piece_id in isect_list:
+                    continue
                 # aha! The peer has this piece! Request it.
                 # which part of the piece do we need next?
                 # (must get the next-needed blocks in order)
                 start_block = self.pieces[piece_id]
                 r = Request(self.id, peer.id, piece_id, start_block)
                 requests.append(r)
+                cur_len += 1
 
         return requests
 
@@ -81,9 +102,9 @@ class MMJWPropshare(Peer):
         In each round, this will be called after requests().
         """
 
-        round = history.current_round()
+        round_num = history.current_round()
         logging.debug("%s again.  It's round %d." % (
-            self.id, round))
+            self.id, round_num))
         # One could look at other stuff in the history too here.
         # For example, history.downloads[round-1] (if round != 0, of course)
         # has a list of Download objects for each Download to this peer in
@@ -98,10 +119,48 @@ class MMJWPropshare(Peer):
             # change my internal state for no reason
             self.dummy_state["cake"] = "pie"
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+            # declare propshare constants
+            OPTIM_UNCHOKE_RATIO = 0.1 # ratio of bw to use for optim unchoke
+
+            # check previous round for who we downloaded from
+            prev_round_dt = history.downloads[round_num - 1]
+            prev_rnd_ids = []
+            block_sizes = []
+            for dl_obj in prev_round_dt:
+                prev_rnd_ids.append(dl_obj.from_id)
+                block_sizes.append(dl_obj.blocks)
+            prev_rnd_bw = dict(zip(prev_rnd_ids, block_sizes))
+            
+            chosen_req = [] # stores which ones were in prev round
+            chosen_bws = []
+            optim_candidates = [] # for optimistic unchoking
+            for cur_req in requests:
+                if cur_req.requester_id in prev_rnd_ids:
+                    chosen_req.append(cur_req.requester_id)
+                    chosen_bws.append(prev_rnd_bw[cur_req.requester_id])
+                else:
+                    optim_candidates.append(cur_req.requester_id)
+
+            tot_bws = sum(chosen_bws)
+            # So we're supposed to floor these (source: Ed PS2 Megathread)
+            adjusted_bws_sum = 0
+            if len(optim_candidates) == 0:
+                OPTIM_UNCHOKE_RATIO = 0
+            for i in range(len(chosen_bws)):
+                chosen_bws[i] *= self.up_bw * (1 - OPTIM_UNCHOKE_RATIO)
+                chosen_bws[i] /= tot_bws
+                chosen_bws[i] = floor(chosen_bws[i])
+                adjusted_bws_sum += chosen_bws[i]
+            if len(optim_candidates) > 0:
+                chosen_req.append(random.choice(optim_candidates))
+                chosen_bws.append(floor(self.up_bw * OPTIM_UNCHOKE_RATIO))
+                adjusted_bws_sum += chosen_bws[-1]
+            rem = self.up_bw - adjusted_bws_sum
+            for i in random.choices(range(len(chosen_bws)), k = max(0, rem)):
+                chosen_bws[i] += 1
+
+            chosen = chosen_req
+            bws = chosen_bws
 
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
